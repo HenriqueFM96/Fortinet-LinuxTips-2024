f742ab7731a4cce5936fc106860a0c6a29e00a0b
HenriqueFM96 <henriquefm96@hotmail.com>
2024-07-12 14:35:51 -0300

    custom app

--- azure-initial-infra.tf
+++ azure-initial-infra.tf
@@ -315,11 +315,11 @@ resource "azurerm_virtual_machine_extension" "os-config" {
 
   settings = <<SETTINGS
  {
-  sudo apt-get install net-tools -y
-  sudo apt-get install docker, python, pip -y
-  mkdir ./log4shell-PoC
-  cd ./log4shell-PoC
-  sudo git clone https://github.com/kozmer/log4j-shell-poc
+  :sudo apt-get install net-tools -y:
+  :sudo apt-get install docker, python, pip -y:
+  :mkdir ./log4shell-PoC:
+  :cd ./log4shell-PoC:
+  :sudo git clone https://github.com/kozmer/log4j-shell-poc:
  }
 SETTINGS
 }
