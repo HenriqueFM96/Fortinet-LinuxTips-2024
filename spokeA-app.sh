0b8dbf05855503ac57b0248571b97991f4b38e6a
HenriqueFM96 <henriquefm96@hotmail.com>
2024-07-12 15:12:15 -0300

    custom app

--- spokeA-app.sh
+++ spokeA-app.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-sudo apt install net-tools -y
-sudo apt install docker, python, pip -y
+sudo apt-get install net-tools -y
+sudo snap install docker
+sudo apt-get install python3 -y
 mkdir ./log4shell-PoC
 cd ./log4shell-PoC
+sudo python3 -m pip install --upgrade pip setuptools wheel
 sudo git clone https://github.com/kozmer/log4j-shell-poc
